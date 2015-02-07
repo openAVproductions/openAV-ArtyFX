4c8c9fa72f8d5ebcff8df9c7f833c41e90ac3a98
Harry van Haaren <harryhaaren@gmail.com>
2015-02-07 12:36:19 +0000

    -Debug printing macros

---
+++ avtk/common.hxx
@@ -0,0 +1,43 @@
+/*
+ * Author: Harry van Haaren 2015
+ *         harryhaaren@gmail.com
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPENAV_AVTK_COMMON_HXX
+#define OPENAV_AVTK_COMMON_HXX
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#ifndef OPENAV_NAME
+#define NAME "Avtk"
+#endif
+
+enum DEBUG_LEVEL {
+  DEBUG_LEVEL_NOTE = 0,
+  DEBUG_LEVEL_WARN,
+  DEBUG_LEVEL_ERROR,
+};
+
+void avtk_debug( int warnLevel, const char* name, const char* file, const char* func, int line,
+                  const char* format = 0, ... );
+
+#define AVTK_NOTE( format, args... ) avtk_debug( DEBUG_LEVEL_NOTE, NAME, __FILE__, __FUNCTION__, __LINE__, format, ## args )
+#define AVTK_WARN( format, args... ) avtk_debug( DEBUG_LEVEL_WARN, NAME, __FILE__, __FUNCTION__, __LINE__, format, ## args )
+#define AVTK_ERROR( format, args... ) avtk_debug( DEBUG_LEVEL_ERROR, NAME, __FILE__, __FUNCTION__, __LINE__, format, ## args )
+
+#endif // OPENAV_AVTK_COMMON_HXX
+
