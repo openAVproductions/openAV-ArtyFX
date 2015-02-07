5683012f6663e854b151bd794fb43910709b4f4a
Harry van Haaren <harryhaaren@gmail.com>
2015-02-07 12:35:59 +0000

    -List test improvements

--- tests/list_test.cxx
+++ tests/list_test.cxx
@@ -25,7 +25,7 @@ ListUI::ListUI(PuglNativeWindow parent) :
   listH->resizeMode( Group::RESIZE_FIT_TO_CHILDREN );
   
   Avtk::Widget* w = 0;
-  for(int i = 0; i < 2; i++ )
+  for(int i = 0; i < 8; i++ )
   {
     listH->addItem( "Tst-H" );
   }
@@ -34,13 +34,13 @@ ListUI::ListUI(PuglNativeWindow parent) :
   listV = new Avtk::List( this, 10, 10, 40, 40, "ListV" );
   listV->mode( Group::WIDTH_EQUAL );
   listV->resizeMode( Group::RESIZE_FIT_TO_CHILDREN );
-  /*
+  
   w = 0;
   for(int i = 0; i < 5; i++ )
   {
     listV->addItem( "Tst-V" );
   }
-  */
+  
   
   listH->callback = cb;
   listV->callback = cb;
