44e28ff826255c56a235cd1ebc17bd434ba1bb49
Harry van Haaren <harryhaaren@gmail.com>
2015-05-19 21:12:10 +0100

    #13 Added Roomy UI

---
+++ src/ui/roomy.hxx
@@ -0,0 +1,26 @@
+
+#ifndef OPENAV_ARTYFX_ROOMY_UI_HXX
+#define OPENAV_ARTYFX_ROOMY_UI_HXX
+
+#include "../avtk/avtk.hxx"
+
+namespace Avtk
+{
+  class Widget;
+};
+
+class RoomyUI : public Avtk::UI
+{
+  public:
+    RoomyUI(PuglNativeWindow parent = 0);
+    virtual ~RoomyUI(){}
+    
+    // ignore widget values, empty function body
+    virtual void widgetValueCB( Avtk::Widget* widget){}
+    
+    Avtk::Dial* dial1;
+    Avtk::Dial* dial2;
+};
+
+
+#endif // OPENAV_AVTK_DIAL_TEST_HXX
