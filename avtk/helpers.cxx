af6941d3e16b91e9e00c67820ecab7de80a970bc
Harry van Haaren <harryhaaren@gmail.com>
2015-06-30 16:30:03 +0100

    Fixed warnings with helpers.hxx

---
+++ avtk/helpers.cxx
@@ -0,0 +1,24 @@
+
+#include "helpers.hxx"
+
+void roundedBox(cairo_t* cr, double x, double y, double w, double h, double radius )
+{
+  static const double degrees = 3.14159265 / 180.0;
+
+  cairo_new_sub_path(cr);
+  cairo_arc(cr,
+            x + w - radius,
+            y + radius,
+            radius, -90 * degrees, 0 * degrees);
+  cairo_arc(cr,
+            x + w - radius, y + h - radius,
+            radius, 0 * degrees, 90 * degrees);
+  cairo_arc(cr,
+            x + radius, y + h - radius,
+            radius, 90 * degrees, 180 * degrees);
+  cairo_arc(cr,
+            x + radius, y + radius,
+            radius, 180 * degrees, 270 * degrees);
+  cairo_close_path(cr);
+}
+
