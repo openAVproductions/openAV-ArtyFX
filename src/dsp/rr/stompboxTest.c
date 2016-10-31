134f3bb011d58afb8173568846c494697c81b1ec
Harry van Haaren <harryhaaren@gmail.com>
2016-10-31 18:23:33 +0000

    dsp: stompboxTest update

--- src/dsp/rr/stompboxTest.c
+++ src/dsp/rr/stompboxTest.c
@@ -1,5 +1,5 @@
 
-// g++ AnalogFilter.C StompBox.C Waveshaper.C stompboxTest.c
+// g++ AnalogFilter.cxx StompBox.cxx Waveshaper.cxx stompboxTest.c
 
 #include "StompBox.h"
 
@@ -8,8 +8,11 @@ int main()
 
 	StompBox s(44100);
 
-	//float tmp[128];
-	//s.process( 128, tmp, tmp );
+	float tmp[128];
+	for(int i = 0; i < 128; i++)
+		tmp[i] = i;
+
+	s.process( 128, tmp, tmp );
 
 	return 0;
 }
